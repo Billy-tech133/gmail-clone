6c17d990c1a8e62c9a4d881271d16766f7df132a
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-08-10 10:21:45 +0100

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,46 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
-import { Add, Inbox } from "@material-ui/icons";
-import { Button } from "@material-ui/core";
+import {
+  AccessTime,
+  Add,
+  ExpandMore,
+  Inbox,
+  LabelImportant,
+  NearMe,
+  Star,
+  Note,
+  Person,
+  Duo,
+  Phone,
+} from "@material-ui/icons";
+import { Button, IconButton } from "@material-ui/core";
 function Sidebar() {
   return (
     <div className="sidebar">
       <Button startIcon={<Add fontSize="large" />} className="sidebar-compose">
         Compose
       </Button>
-      <SidebarOption Icon={Inbox} title="Inbox" number={54} />
+      <SidebarOption selected={true} Icon={Inbox} title="Inbox" number={54} />
+      <SidebarOption Icon={Star} title="Starred" number={54} />
+      <SidebarOption Icon={AccessTime} title="Snozzed" number={54} />
+      <SidebarOption Icon={LabelImportant} title="Important" number={54} />
+      <SidebarOption Icon={NearMe} title="Sent" number={54} />
+      <SidebarOption Icon={Note} title="Drafts" number={54} />
+      <SidebarOption Icon={ExpandMore} title="More" number={54} />
+      <div className="sidebar-footer">
+        <div className="sidebar-foort">
+          <IconButton>
+            <Person />
+          </IconButton>
+          <IconButton>
+            <Duo />
+          </IconButton>
+          <IconButton>
+            <Phone />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
